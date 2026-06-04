916b270ddc020d3630f81a7cee82945947044931
YashashavGoyal <YashashavGoyal@gmail.com>
2026-06-04 15:23:27 +0530

    fix: add env var for api

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,10 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   async rewrites() {
+    let apiUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000';
+    if (!apiUrl.endsWith('/api/:path*')) {
+      apiUrl = apiUrl.replace(/\/$/, '') + '/api/:path*';
+    }
     return [
       {
         source: '/api/:path*',
-        destination: 'http://127.0.0.1:5000/api/:path*',
+        destination: apiUrl,
       },
     ];
   },
